86eb611cf62c15f74e70fb6cdf5b356f6dd30e2e
David Cinco <cincoisc@gmail.com>
2026-08-28 20:36:38 -0600

    changes in the test for customer

--- tests/customer/customer-test.spec.ts
+++ tests/customer/customer-test.spec.ts
@@ -2,64 +2,34 @@ import { test, expect } from '@playwright/test';
 import { CustomerPage } from '../../pages/CustomerPage.ts';
 
 test.describe('Add Customer: Testing - Suite', () => {
+  //adding a global variable for each test
+  let customerPage: CustomerPage;
+
+  //Implementing a beforeEach Hook to setup each test case
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');    
+    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
+    await page.getByRole('button', { name: 'Add Customer' }).click();
+    customerPage = new CustomerPage(page);
+  });
 
   test('Adding new customer with correct values', async ({ page }) => {
-    await page.goto('');
-    
-    // Click on Customer Login Button
-    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
-    // Click on Add Customer Button
-    await page.getByRole('button', { name: 'Add Customer' }).click();
-    // Create object of Customer Login Page
-    const customerPage = new CustomerPage(page);
     await customerPage.adding_new_customer("Albert", "Wesker", "9901");  
   });
 
   test('Adding new customer with Empty First Name Value', async ({ page }) => {
-    await page.goto('');
-    
-    // Click on Customer Login Button
-    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
-    // Click on Add Customer Button
-    await page.getByRole('button', { name: 'Add Customer' }).click();
-    // Create object of Customer Login Page
-    const customerPage = new CustomerPage(page);
     await customerPage.adding_new_customer("", "Wesker", "9901");  
   });
 
   test('Adding new customer with Empty Last Name Value', async ({ page }) => {
-    await page.goto('');
-    
-    // Click on Customer Login Button
-    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
-    // Click on Add Customer Button
-    await page.getByRole('button', { name: 'Add Customer' }).click();
-    // Create object of Customer Login Page
-    const customerPage = new CustomerPage(page);
     await customerPage.adding_new_customer("Albert", "", "9901");  
   });
 
   test('Adding new customer with Empty Post Code Value', async ({ page }) => {
-    await page.goto('');
-    
-    // Click on Customer Login Button
-    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
-    // Click on Add Customer Button
-    await page.getByRole('button', { name: 'Add Customer' }).click();
-    // Create object of Customer Login Page
-    const customerPage = new CustomerPage(page);
     await customerPage.adding_new_customer("Albert", "Wesker", "");  
   });
 
   test('Adding new customer with All Empty Fields', async ({ page }) => {
-    await page.goto('');
-    
-    // Click on Customer Login Button
-    await page.getByRole('button', { name: 'Bank Manager Login' }).click();
-    // Click on Add Customer Button
-    await page.getByRole('button', { name: 'Add Customer' }).click();
-    // Create object of Customer Login Page
-    const customerPage = new CustomerPage(page);
     await customerPage.adding_new_customer("", "", "");
   });
 
